29ad6f7ce94dadcc827072846a6eac80d9eeb828
Knock <enoch10evelyn@gmail.com>
2022-10-02 13:43:13 -0700

    Questions edited email and user name added

--- index.js
+++ index.js
@@ -4,12 +4,41 @@ const inquirer = require("inquirer");
 
 inquirer
   .prompt([
+    //Github UserName
+    {
+      type: "input",
+      name: "GitHub Name",
+      message: "Please enter your GitHub username",
+      //validates if the input is provided and acceptable
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          console.log("Please enter your Github ussername");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "Email",
+      message: "Please enter your email address",
+      //validates if the input is provided and acceptable
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          console.log("Please enter your email address");
+          return false;
+        }
+      },
+    },
     //title
     {
       type: "input",
       name: "title",
       message: "What's is the project title ?",
-      //validates if the input is provided and acceptable 
+      //validates if the input is provided and acceptable
       validate: (titleInput) => {
         if (titleInput) {
           return true;
@@ -19,12 +48,12 @@ inquirer
         }
       },
     },
-    //descripton 
+    //descripton
     {
       type: "input",
       name: "Description",
       message: "Describe your project",
-      //validates if the input is provided and acceptable 
+      //validates if the input is provided and acceptable
       validate: (value) => {
         if (value) {
           return true;
@@ -39,16 +68,15 @@ inquirer
       type: "checkbox",
       name: "License",
       message: "What badges does your project have?",
-      choices:[ 
+      choices: [
         "Tori",
         "AssetSonar",
         "SofTrack",
         "Zylo",
         "Zentitle",
-        "Alloy Navigator"
-
+        "Alloy Navigator",
       ],
-      //validates if the input is provided and acceptable 
+      //validates if the input is provided and acceptable
       validate: (value) => {
         if (value) {
           return true;
@@ -58,12 +86,12 @@ inquirer
         }
       },
     },
-    //installation 
+    //installation
     {
       type: "input",
       name: "Installation",
       message: "What command should be ran to install dependencies?",
-      //validates if the input is provided and acceptable 
+      //validates if the input is provided and acceptable
       validate: (value) => {
         if (value) {
           return true;
@@ -78,7 +106,7 @@ inquirer
       type: "input",
       name: "Usage",
       message: "Please explain how this project can be used.",
-      //validates if the input is provided and acceptable 
+      //validates if the input is provided and acceptable
       validate: (value) => {
         if (value) {
           return true;
@@ -88,31 +116,17 @@ inquirer
         }
       },
     },
-    //contributing  
+    //contributing
     {
       type: "input",
       name: "Contributing",
       message: "How can users contribute to your project",
-      //validates if the input is provided and acceptable 
+      //validates if the input is provided and acceptable
       validate: (value) => {
         if (value) {
           return true;
         } else {
           console.log("Please enter a short description");
-          return false;
-        }
-      },
-    },
-    {
-      type: "input",
-      name: "Description",
-      message: "Describe your project",
-      //validates if the input is provided and acceptable 
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          console.log("Please enter a short project description");
           return false;
         }
       },
