486ee404fbd772ec9e180c61b4ecef4bb0caf536
Knock <enoch10evelyn@gmail.com>
2022-10-02 12:54:02 -0700

    Title prompt and validate

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-console.log('Hello Node!');
-//node modules 
+//node modules
 const inquirer = require("inquirer");
-const fs = require("fs");
+//const fs = require("fs");
 
-//inquirer .prompt 
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What's is the project title ?",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
+    },
+  ])
+  .then((answers) => console.log(answers));
