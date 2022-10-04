a329fd7307f1b2032e6f8ea1294f79b5f696dec6
Knock <enoch10evelyn@gmail.com>
2022-10-03 21:44:32 -0700

    Refactored template and module exported

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Read-Me-Generator-
+# r
+     ![License Badge](https://img.shields.io/badge/license-GPL 3.0-red)
+     
+    # Description
+
+    e 
+    
+    ## Table of Contents
+ 
+    * [Description](#description)
+    * [Installation Guide](#installation)
+    * [Usage](#usage)
+    * [Test Instructions](#test)
+    * [Contributing](#contributing)
+    * [License](#license)
+    
+    ## Installation
+
+    rdata.
+
+    ## Test Instructions
+
+    e
+
+    ## Usage
+
+    e
+
+    ## Contributing
+
+    r
+
+    ## Questions
+     
+    If you have any questions about the project you can contact me directly via e. 
+
+    You can find more of my work at [undefined](#https://github.com/undefined)   
+    
--- utils/generatemarkdown.js
+++ utils/generatemarkdown.js
@@ -1,58 +1,67 @@
-function renderLicenseBadge(license){
-    if(license !== "None"){
-        return `![License Badge](https://img.shields.io/badge/license-${license}-red)`
-    }
-    return "";
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![License Badge](https://img.shields.io/badge/license-${license}-red)`;
+  }
+  return "";
 }
 
-function renderLicenseLink(license){
-    if(license !== "None"){
-        return `* [License](#license)`
-    }
-    return "";
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `* [License](#license)`;
+  }
+  return "";
 }
 
-function renderLicenseSection(license){
-    if(license !== "None"){
-        return `## License
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License
         
-        this project is licensed under the ${license} license.`
-    }
-    return "";
+        This project is licensed under the ${license} license.`;
+  }
+  return "";
 }
 
+function generatemarkdown(data) {
+  return `# ${data.title}
+     ${renderLicenseBadge(data.license)}
+     
+    # Description
 
-
-
-const generateReadME = `# ${title}
- ## ${license}
-## Table of Contents
+    ${data.description} 
+    
+    ## Table of Contents
  
- * [Description](#description)
- * [Installation Guide](#installation)
- * [Usage](#usage)
- * [Test Instructions](#test)
- * [Contributing](#contributing)
- * [License](#license)
- 
-# Description
-${description}
+    * [Description](#description)
+    * [Installation Guide](#installation)
+    * [Usage](#usage)
+    * [Test Instructions](#test)
+    * [Contributing](#contributing)
+    ${renderLicenseLink(data.license)}
+    
+    ## Installation
 
-## Installation
-${installation}
+    ${data.installation}data.
 
-## Test Instructions
-${test}
+    ## Test Instructions
 
-## Usage
-${usage}
+    ${data.test}
 
-## Contributing
-${contribution}
+    ## Usage
 
+    ${data.usage}
 
+    ## Contributing
 
-## Questions
- *[${github}](#https://github.com/${github})
- *${email}
- `;
+    ${data.contribution}
+
+    ## Questions
+     
+    If you have any questions about the project you can contact me directly via ${
+      data.email
+    }. 
+
+    You can find more of my work at [${data.github}](#https://github.com/${
+    data.github})   
+    `;
+}
+module.exports = generatemarkdown;
